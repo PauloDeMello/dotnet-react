74314238ca664543d690dc94468ae7dcc71060ee
pdemello13 <paulo.demello@kagenova.com>
2024-06-07 16:15:28 +0100

    Install axios and to package-lock.json, set up React app to use axios to fetch data from .net server API. Set up CORS in .net server API to allow react app to use .net server API

--- Reactivities/client-app/src/App.tsx
+++ Reactivities/client-app/src/App.tsx
@@ -1,14 +1,27 @@
+import { useEffect, useState } from 'react'
 import './App.css'
-import DuckItem from './DuckItem'
-import { ducks } from './demo'
+import axios from 'axios';
 
 function App() {
+  const [activities, setActivities] = useState([]); { /*First param is value, second is the function used to set the value, [] means no initial states*/ }
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities')
+      .then(responce => {
+        setActivities(responce.data);
+      })
+  }, []); { /*Second parameter are the dependencies, so when a dependency changes, the useEffect method gets called again */ }
+
   return (
     <div>
       <h1>Reactivities</h1>
-      {ducks.map(duck => (
-        <DuckItem duck={duck} />
-      ))}
+      <ul>
+        {activities.map((activity: any) => (
+          <li key={activity.id}>
+            {activity.title};
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
