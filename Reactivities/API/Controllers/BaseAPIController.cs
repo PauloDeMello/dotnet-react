9cc9d45f8431d86d39f9f3eadec58c45d96b7044
pdemello13 <paulo.demello@kagenova.com>
2024-06-07 11:24:05 +0100

    Create new controller for activities entity, with two get entry points, and add comments to default controller

---
+++ Reactivities/API/Controllers/BaseAPIController.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] //uses name of controller for route i.e localhost:5000/weatherforecast
+    public class BaseAPIController
+    {
+
+    }
+}
