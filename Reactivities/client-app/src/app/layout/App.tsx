ba0b60462cacbdceaf7f1da6ca37ac18fbb29467
pdemello13 <paulo.demello@kagenova.com>
2024-06-12 14:15:20 +0100

    Set up basic react styling for navbar / components

--- Reactivities/client-app/src/app/layout/App.tsx
+++ Reactivities/client-app/src/app/layout/App.tsx
@@ -1,28 +1,33 @@
-import { useEffect, useState } from 'react'
+import { Fragment, useEffect, useState } from 'react'
 import axios from 'axios';
-import { Header, List } from 'semantic-ui-react';
+import { Container, Header, List } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
+import NavBar from './navbar';
 
 function App() {
-  const [activities, setActivities] = useState([]); { /*First param is value, second is the function used to set the value, [] means no initial states*/ }
+  const [activities, setActivities] = useState<Activity[]>([]); { /*First param is value, second is the function used to set the value, [] means no initial states*/ }
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/activities')
+    axios.get<Activity[]>('http://localhost:5000/api/activities')
       .then(responce => {
         setActivities(responce.data);
       })
   }, []); { /*Second parameter are the dependencies, so when a dependency changes, the useEffect method gets called again */ }
 
   return (
-    <div>
-      <Header as="h2" icon='users' content='Reactivities' />
-      <List>
-        {activities.map((activity: any) => (
-          <List.Item key={activity.id}>
-            {activity.title}
-          </List.Item>
-        ))}
-      </List>
-    </div>
+    <Fragment> {/* We can only return one "return styled" component, e.g navbar or container. So we encapsulate all within one component (div/frag). */}
+      <NavBar />
+      <Container style={{ marginTop: '7em' }}>
+        <List>
+          {activities.map((activity) => (
+            <List.Item key={activity.id}>
+              {activity.title}
+            </List.Item>
+          ))}
+        </List>
+      </Container>
+
+    </Fragment>
   )
 }
 
