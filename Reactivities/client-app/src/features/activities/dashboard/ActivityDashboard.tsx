589a233d726d2fe4f4eef45d2c689b3f97e099e0
pdemello13 <paulo.demello@kagenova.com>
2024-07-15 09:57:05 +0100

    Set up mobx to avoid Prop drilling in React, and switch activities list to a Map

--- Reactivities/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ Reactivities/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,50 +1,26 @@
 import React from 'react';
 import { Grid } from 'semantic-ui-react';
-import { Activity } from '../../../app/models/activity';
 import ActivityList from './ActivityList';
 import ActivityDetails from '../details/ActivityDetails';
 import ActivityForm from '../form/ActivityForm';
+import { useStore } from '../../../app/stores/store';
+import { observer } from 'mobx-react-lite';
 
-interface Props {
-    activities: Activity[];
-    selectedActivity: Activity | undefined;
-    selectActivity: (id: string) => void;
-    cancelSelectActivity: () => void;
-    editMode: boolean;
-    openForm: (id: string) => void;
-    closeForm: () => void;
-    createOrEdit: (activity: Activity) => void;
-    deleteActivity: (id: string) => void;
-    submitting: boolean;
+export default observer(function ActivityDashboard() {
+    const { activityStore } = useStore();
+    const { selectedActivity, editMode } = activityStore;
 
-}
-
-export default function ActivityDashboard({ activities, selectedActivity,
-    selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity, submitting }: Props) { /* { activities }: Props destructures prop: Props, so we no longer need prop.activities*/
     return (
         <Grid>
             <Grid.Column width="10">
-                <ActivityList activities={activities}
-                    selectActivity={selectActivity}
-                    deleteActivity={deleteActivity}
-                    submitting={submitting}
-                />
+                <ActivityList />
             </Grid.Column>
             <Grid.Column width="6"> {/*Total grid columns add up to 16*/}
                 {selectedActivity && !editMode &&
-                    <ActivityDetails
-                        activity={selectedActivity}
-                        cancelSelectActivity={cancelSelectActivity}
-                        openForm={openForm}
-                    />} {/*&& means that the code on the right only runs if variable on left is not null*/}
+                    <ActivityDetails />} {/*&& means that the code on the right only runs if variable on left is not null*/}
                 {editMode &&
-                    <ActivityForm
-                        createOrEdit={createOrEdit}
-                        closeForm={closeForm}
-                        activity={selectedActivity}
-                        submitting={submitting}
-                    />}
+                    <ActivityForm />}
             </Grid.Column>
         </Grid>
     )
-}
+})
--- Reactivities/client-app/src/features/activities/form/ActivityForm.tsx
+++ Reactivities/client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,15 +1,13 @@
 import React, { ChangeEvent, useState } from 'react';
 import { Button, Form, Segment } from 'semantic-ui-react';
-import { Activity } from '../../../app/models/activity';
+import { useStore } from '../../../app/stores/store';
+import { observer } from 'mobx-react-lite';
 
-interface Props {
-    activity: Activity | undefined;
-    closeForm: () => void;
-    createOrEdit: (activity: Activity) => void;
-    submitting: boolean;
-}
+//Needs to be an observer since we observing state changes / actions (deleteActivity)
+export default observer(function ActivityForm() {
+    const { activityStore } = useStore();
+    const { selectedActivity, closeForm, createActivity, updateActivity, loading } = activityStore;
 
-export default function ActivityForm({ activity: selectedActivity, closeForm, createOrEdit, submitting }: Props) {
     const initialState = selectedActivity ?? {
         id: '',
         title: '',
@@ -23,7 +21,7 @@ export default function ActivityForm({ activity: selectedActivity, closeForm, cr
     const [activity, setActivity] = useState(initialState);
 
     function handleSubmit() {
-        createOrEdit(activity);
+        activity.id ? updateActivity(activity) : createActivity(activity);
     }
 
     function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
@@ -40,9 +38,9 @@ export default function ActivityForm({ activity: selectedActivity, closeForm, cr
                 <Form.Input type='date' placeholder='Date' value={activity.date} name='date' onChange={handleInputChange} />
                 <Form.Input placeholder='City' value={activity.city} name='city' onChange={handleInputChange} />
                 <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={handleInputChange} />
-                <Button loading={submitting} float='right' positive type='submit' content='Submit' />
+                <Button loading={loading} float='right' positive type='submit' content='Submit' />
                 <Button onClick={() => closeForm()} float='right' type='button' content='Cancel' />
             </Form>
         </Segment>
     )
-}
+})
